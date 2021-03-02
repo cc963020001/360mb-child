a431c7c302377c1f0ef4ca756b0ea769ad5bb835
cc963020001 <963020001@qq.com>
2021-03-03 04:10:22 +0800

    js

---
+++ public.js
@@ -0,0 +1,5 @@
+with(document){
+write('<script type="text/javascript" language="javascript" src="https://cdn.jsdelivr.net/gh/cc963020001/jquery.barrager/dist/js/jquery.barrager.js"></script>');//弹幕插件
+write('<script type="text/javascript" language="javascript" src="https://cdn.jsdelivr.net/gh/imakewebthings/waypoints@4.0.0/lib/jquery.waypoints.js"></script>');//
+write('<script type="text/javascript" language="javascript" src="https://www.360mb.net/wp-content/themes/360mb-child/assets/js/banner.js"></script>');//幻灯插件
+} 
